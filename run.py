38997ee913774b83da9751b803e4683240b77b53
Duvanqx <Arlexquiceno@gmail.com>
2025-07-10 16:51:42 -0500

    Run.py solucionado

--- run.py
+++ run.py
@@ -1,13 +1,11 @@
 import os
-from app import create_app
-
+from app import create_app, db  
 app = create_app()
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0',port=port)
-
-from app import db
 
 with app.app_context():
     db.create_all()
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
